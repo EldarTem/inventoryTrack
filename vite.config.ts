80498378a41427179ad7e171e06470ddc6fc20cf
Eldar Temirov <temirov.e.i.21@gmail.com>
2025-05-19 01:02:29 +0300

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
@@ -19,6 +17,18 @@ export default defineConfig({
         target: 'http://194.67.84.131:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // Убедитесь, что сборка идет в папку dist
+    sourcemap: true, // Для отладки
+    rollupOptions: {
+      output: {
+        // Убедитесь, что выходные файлы имеют расширение .js
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
       },
     },
   },
